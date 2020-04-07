3f6e2a511c7eb30ce094e120776ad51d20a0ed46
dev-augustin <dev.augustin00@gmail.com>
2020-04-07 18:51:39 -0500

    added component files to App.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router, Switch, Route, Link
+} from "react-router-dom";
+import Sunday from './components/Sunday';
+import Monday from './components/Monday';
+import Tuesday from './components/Tuesday';
+import Wednesday from './components/Wednesday';
+import Thursday from './components/Thursday';
+import Friday from './components/Friday';
+import Saturday from './components/Saturday';
 
 console.log(process.env.REACT_APP_API_KEY)
 
 function App() {
   return (
     <div className="App">
-    
+         
     </div>
   );
 }
