b7fe9ae7d97423a7d07b253ea43b724ab6ec53fe
dev-augustin <dev.augustin00@gmail.com>
2020-04-08 16:31:44 -0500

    added env.key

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,56 +2,40 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import {
-  BrowserRouter as Router, Switch, Route, Link
+  BrowserRouter as Router, Switch,Route, Link
 } from "react-router-dom";
 import Sunday from './components/Sunday';
-import Monday from './components/Monday';
-import Tuesday from './components/Tuesday';
-import Wednesday from './components/Wednesday';
-import Thursday from './components/Thursday';
-import Friday from './components/Friday';
-import Saturday from './components/Saturday';
-
-console.log(process.env.REACT_APP_API_KEY)
+// import Monday from './components/Monday';
+// import Tuesday from './components/Tuesday';
+// import Wednesday from './components/Wednesday';
+// import Thursday from './components/Thursday';
+// import Friday from './components/Friday';
+// import Saturday from './components/Saturday';
+import Weather from './components/Weather';
+ //console.log(process.env.REACT_APP_API_KEY)
 
 function App() {
   return (
-    <Router>
-      <div>
-        {/* <ul> */}
-          {/* <li> */}
-            <Link to='/Sunday'>Sunday</Link> |
-          {/* </li> */}
-          {/* <li> */}
-            <Link to='/Monday'>Monday</Link> |
-          {/* </li> */}
-          {/* <li> */}
-            <Link to='/Tuesday'>Tuesday</Link> |
-          {/* </li> */}
-          {/* <li> */}
-            <Link to='/Wednesday'>Wednesday</Link> |
-          {/* </li> */}
-          {/* <li> */}
-            <Link to='/Thursday'>Thursday</Link>   |
-          {/* </li> */}
-          {/* <li> */}
-            <Link to='/Friday'>Friday</Link> |
-          {/* </li> */}
-          {/* <li> */}
-            <Link to='/Saturday'>Saturday</Link> |
-          {/* </li> */}
-        {/* </ul> */}
-
-
-        <Switch>
-            <Route path='/Sunday'>
-              <Sunday/>
+      <Router>
+           
+               <Weather/>
+         
+              <Link to="/Sunday">Sunday</Link>  |
+              {/* <Link to="/Monday">Monday</Link>   |
+              <Link to="/Tuesday">Tuesday</Link>  |
+              <Link to="/Wednesday">Wednesday</Link> |
+              <Link to="/Thursday">Thursday</Link> |
+              <Link to="/Friday">Friday</Link> |
+              <Link to="/Saturday">Saturday</Link>  */}
+           <Switch>
+            <Route path='/Sunday' component={Sunday}/>
+              {/* <Sunday/> */}
+            {/* </Route> */}
+            {/* <Route path='/Monday' component={Monday}>
+            
             </Route>
-            <Route path='/Monday'>
-              <Monday/>
-            </Route>
-            <Route path='/Tuesday'>
-              <Tuesday/>
+            <Route path='/Tuesday' component={Tuesday}>
+             
             </Route>
             <Route path='/Wednesday'>
               <Wednesday/>
@@ -64,9 +48,11 @@ function App() {
             </Route>
             <Route path='/Saturday'>
               <Saturday/>
-            </Route>
-        </Switch>
-      </div>
+            </Route> */}
+           </Switch>
+           
+        {/* </Switch> */}
+      {/* </div> */}
     </Router>
   );
 }
